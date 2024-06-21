413dda806548b07504f9be84b7a3988d0ba59ea9
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-21 18:24:09 +0530

    parse code using esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,49 @@
-import React from 'react-dom';
-import ReactDOM from 'react-dom/client';
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom/client";
 
 const App = () => {
-    return <h1>hi</h1>
-}
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(<App />);
