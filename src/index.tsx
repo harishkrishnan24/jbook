6a92110f646f719616189e51f4a0df7a115683f0
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-01 11:38:14 +0530

    execute code inside iframe

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as esbuild from "esbuild-wasm";
 import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +23,8 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -33,8 +35,28 @@ const App = () => {
         global: "window",
       },
     });
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener("message", (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            const root = document.getElementById("root");
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+           }
+        }, false);
+      </script>
+    </body>
+  </html>
+`;
 
   return (
     <div>
@@ -45,7 +67,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} title="preview"></iframe>
     </div>
   );
 };
