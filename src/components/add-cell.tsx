68104499ff2d9b91c932bddc90828252aebbc26a
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-07-15 13:32:30 +0530

    add cell component

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,42 @@
+import { useActions } from "../hooks/use-actions";
+import "./add-cell.css";
+
+interface AddCellProps {
+  nextCellId: string | null;
+  forceVisible?: boolean;
+}
+
+const AddCell: React.FC<AddCellProps> = ({
+  nextCellId,
+  forceVisible = false,
+}) => {
+  const { insertCellBefore } = useActions();
+
+  return (
+    <div className={`add-cell ${forceVisible ? "force-visible" : ""}`}>
+      <div className="add-buttons">
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, "CODE")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Code</span>
+        </button>
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellId, "TEXT")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus"></i>
+          </span>
+          <span>Text</span>
+        </button>
+      </div>
+      <div className="divider"></div>
+    </div>
+  );
+};
+
+export default AddCell;
